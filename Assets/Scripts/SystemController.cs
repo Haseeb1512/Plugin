e07bd77a6796818ceec0b0c621f43f33bf00bc29
Haseeb <syedhaseebraza12@gmail.com>
2025-10-19 19:30:42 +0500

    First Commit

---
+++ Assets/Scripts/SystemController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SystemController", menuName = "Scriptable Objects/SystemController")]
+public class SystemController : ScriptableObject
+{
+    
+}
